bae6f91402cfd1fdc0bcd56f36dc5d385f628495
zhaoyuehrb <zhaoy@m219502401.imcb.a-star.edu.sg>
2019-11-27 18:02:50 +0800

    deseq base t1 and AUG range edit

--- Deseq2 Analysis/shift15/coding/de_te_ana_indriks_design.R
+++ Deseq2 Analysis/shift15/coding/de_te_ana_indriks_design.R
@@ -24,28 +24,7 @@ filter_sig_res <- function(dds, factors) {
   
   summary(res_df)
   res_df <- na.omit(res_df)
-  # #
-  # res_df$SYMBOL <- mapIds(org.Mm.eg.db,
-  #                         keys = as.character(rownames(res_df)),
-  #                         column = 'SYMBOL',
-  #                         keytype = 'ACCNUM',
-  #                         multiVals = 'first')
-  # ###remove statistically insignificant genes
-  # res_df <- res_df[res_df$padj<alpha,]
-  # res_df <- data.frame(ACCNUM=row.names(data.frame(res_df)), res_df,row.names = NULL)
-  # # #
-  # ###select columns Accnum and Log2FC
-  # res_df <- res_df[,c("ACCNUM","log2FoldChange")]
-  # res_df <- data.frame(res_df, row.names=NULL)
-  # # 
-  # # ###select AccNum, log2FC and padj
-  # # res_df <- data.frame(ACCNUM = row.names(data.frame(res_df)), res_df, row.names = NULL)
-  # # res_df <- res_df[,c("ACCNUM","log2FoldChange","padj")]
-  
-  ###add symbol and select padj
-  # res_df <- data.frame(res_df)
-  # rownames(res_df) <- NULL
-  # res_df <- res_df[,c("SYMBOL","log2FoldChange")]
+ 
 }
 
 ###deseq at individual time points for TE, controlling for the baseline
@@ -54,3 +33,41 @@ res_2_te_0 <- filter_sig_res(dds, c("assayRPF.timet2"))
 
 write.csv(res_1_te_0,"csv_raw/TE_t1_t0.csv")
 write.csv(res_2_te_0,"csv_raw/TE_t2_t0.csv")
+
+
+
+
+
+
+
+
+colData$time <- factor(colData$time, levels = c("t1","t2","t0"))
+dds <- DESeqDataSetFromMatrix(countData = df_te, colData = colData,design = design)
+
+# ###run analysis to produce genes which at one or more time points after time 0 showed a TE effect
+# reduced = ~ assay + time
+# dds <- DESeq(dds, test='LRT', reduced = reduced)
+
+#run DESeq
+dds <- DESeq(dds)
+
+alpha <- 0.05
+res <- results(dds,alpha = alpha)
+#res <- results(dds,alpha = alpha, filterFun = IHW)
+resultsNames(dds)
+
+###function to compare different combinations of factors
+filter_sig_res <- function(dds, factors) {
+  res_df <- results(dds, contrast = list(factors),test = "Wald",alpha = alpha,cooksCutoff = FALSE,independentFiltering = FALSE)
+  
+  summary(res_df)
+  res_df <- na.omit(res_df)
+  
+}
+
+###deseq at individual time points for TE, controlling for the baseline
+res_2_te_1 <- filter_sig_res(dds, c("assayRPF.timet2"))
+
+write.csv(res_2_te_1,"csv_raw/TE_t2_t1.csv")
+
+
