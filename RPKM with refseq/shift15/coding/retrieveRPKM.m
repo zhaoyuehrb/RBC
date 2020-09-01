7f6190bbde8c4ac89744dcc89f907cb468277c99
Zhao Yue <zhaoyuehrb@gmail.com>
2020-09-01 22:15:00 +0800

    cluster plots

--- RPKM with refseq/shift15/coding/retrieveRPKM.m
+++ RPKM with refseq/shift15/coding/retrieveRPKM.m
@@ -1,3 +1,5 @@
+T = sortrows(T,[3,4]);
+
 %% add file name column to T
 
 rpkmFilName = cell(30,1);
@@ -28,9 +30,22 @@ end
 
 %% cdReads filter
 
-idxs = sum((cdReadsTable{:,2:end}>=8)') == 30;
+idxs = sum((cdReadsTable{:,2:end}>=5)') == 30;
 disp(['num of genes pass filter: ' num2str(sum(idxs))])
 
 cdReadsTableFiltered = cdReadsTable(idxs,:);
-writetable(cdReadsTableFiltered,'cdReadsFiltered8.csv')
+%writetable(cdReadsTableFiltered,'cdReadsFiltered8.csv')
+
 %writetable(cdReadsTableFiltered,'cdReadsNotFiltered.csv');
+
+cdRPKMTableFiltered = cdRPKMTable(idxs,:);
+%writetable(cdRPKMTableFiltered,'cdRPKMTableFiltered8.csv')
+
+rpkm = table();
+rpkm.AccNum = cdRPKMTable.AccNum;
+cols = cdRPKMTable.Properties.VariableNames;
+for i = 2:2:30
+    colname = cols{i}(1:end-2)
+    rpkm.(colname) = (cdRPKMTable{:,i} + cdRPKMTable{:,i+1} ) / 2;
+end
+writetable(rpkm,'cdRPKMCombFil5.csv')
